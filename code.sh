e559b176508750858086ca44390552097b6e103d
hvk-creator <hvk261208@proton.me>
2025-07-03 16:26:18 +0700

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,4 +1,3 @@
 wget https://github.com/coder/code-server/releases/download/v4.101.2/code-server-4.101.2-linux-amd64.tar.gz
 tar -xf code-server-4.101.2-linux-amd64.tar.gz
 mv code-server-4.101.2-linux-amd64 code
-bash code/bin/code-server --auth none --port 8080 --cert
